460cfdb8a8373719f10a2742ae6a5b15f0332059
vobobos <vebjorn.landmark@outlook.com>
2023-11-23 13:37:58 +0100

    des car

--- car.py
+++ car.py
@@ -1,5 +1,5 @@
 class car:
-    def __init__(self, registNr, bilModell, bilType, kopeAar, bilEier) -> None:
+    def __init__(self, registNr, bilModell, bilType, kopeAar, bilEier):
         self.skilt  = registNr
         self.modell = bilModell
         self.type   = bilType
