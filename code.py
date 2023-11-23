201d30f151d8735078a1f2c2e58d6d8ee24e13e2
vobobos <vebjorn.landmark@outlook.com>
2023-11-23 13:35:44 +0100

    fuck all yaal

--- code.py
+++ code.py
@@ -1,3 +1,11 @@
 # Prosjekt
 # hekkoooooooooooooo
 #many mans could never man
+
+class car:
+    def __init__(self, registNr, bilModell, bilType, kopeAar, bilEier) -> None:
+        self.skilt  = registNr
+        self.modell = bilModell
+        self.type   = bilType
+        self.aar    = kopeAar
+        self.eier   = bilEier
