cda6d92c291faede3c84dfdfefec7955302d11ef
Mathilde Borsodi Hegna <mathilde.hegna@gmail.com>
2023-11-30 12:21:29 +0100

    moahahhahahah

--- car.py
+++
@@ -1,7 +0,0 @@
-class car:
-    def __init__(self, registNr, bilModell, bilType, aarModell, bilEier):
-        self.skilt  = registNr
-        self.modell = bilModell
-        self.type   = bilType
-        self.aar    = aarModell
-        self.eier   = bilEier
